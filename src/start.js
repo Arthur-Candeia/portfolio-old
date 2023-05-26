62279c7fb4b423d433280ce926ec8ba38f302805
Arthur-Candeia <arthurheher@hotmail.com>
2023-05-26 09:08:41 -0300

    melhorias na abertura(start)

--- src/start.js
+++ src/start.js
@@ -3,20 +3,22 @@ export async function load() {
   const startText = document.getElementById('animation-start-text');
   const body = document.querySelector('body')
 
+  body.scroll = 'no'
   startDiv.style.animation = 'growth 4s forwards ease';
   setTimeout(() => {
     startText.style.animation = 'textToApear 1s forwards ease';
-  }, 2000)
+  }, 1550)
 
   setTimeout(() => {
-    startDiv.style.animation = 'toVanish 1.5s forwards ease';
-    startText.style.animation = 'toVanish 1.5s forwards ease';
+    startDiv.style.animation = 'toVanishDiv 1.5s forwards ease';
+    startText.style.animation = 'toVanishText 1.5s forwards ease';
   }, 4000)
 
   setTimeout(() => {
     startDiv.style.display = 'none';
     startText.style.display = 'none';
     body.style.overflow = 'auto';
+    body.scroll = 'yes'
   }, 5500)
 }
 
